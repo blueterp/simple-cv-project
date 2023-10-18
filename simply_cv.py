92c4bad092ef71e9dea946e5fb4ff7bc0353e301
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-10-18 12:55:51 +0100

    initialize repo

---
+++ simply_cv.py
@@ -0,0 +1,26 @@
+from time import sleep
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+sleep(1)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv2.imshow("frame", gray)
+    if cv2.waitKey(1) == ord("q"):
+        break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
