a96aad084a1a5be2c3aa42029633a4f7c39f6440
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-10-23 00:15:35 +0100

    refactor simple cv script used as reference

--- simply_cv.py
+++ simply_cv.py
@@ -3,10 +3,14 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture("vtest.avi")
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+size = (width, height)
+print(size)
 # fourcc = cv2.VideoWriter_fourcc(*"XVID")
-# fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-# out = cv2.VideoWriter("output.avi", fourcc, 20.0, (640, 480))
+fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+out = cv2.VideoWriter("output.avi", fourcc, 20.0, size)
 sleep(1)
 if not cap.isOpened():
     print("Cannot open camera")
@@ -18,9 +22,10 @@ while True:
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    # out.write(frame)
+    out.write(frame)
     cv2.imshow("frame", frame)
-    if cv2.waitKey(3000) == ord("q"):
+    if cv2.waitKey(50) == ord("q"):
         break
 cap.release()
+out.release()
 cv2.destroyAllWindows()
