e6d48928336c027283ec610f54f40547814e454f
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-10-23 21:31:28 +0100

    modify stream and video source interface

--- video_source/stream.py
+++ video_source/stream.py
@@ -3,30 +3,39 @@ from time import time, sleep
 
 
 class Stream:
-    def __init__(self, connection_info=0):
+    def __init__(self, connection_info):
         self.connection_info = connection_info
         self.is_open = False
         self.video_capture = None
 
+        with self:
+            self.width = int(self.video_capture.get(cv.CAP_PROP_FRAME_WIDTH) + 0.5)
+            self.height = int(self.video_capture.get(cv.CAP_PROP_FRAME_HEIGHT) + 0.5)
+
     def __enter__(self):
         self.video_capture = cv.VideoCapture(self.connection_info)
-        self.is_open = self.video_capture.isOpened() if self.video_capture else False
+        if not self.video_capture.isOpened():
+            raise TypeError(
+                "Invalid connection information. Please specify busid or video file"
+            )
+        self.is_open = True
         self.video_capture.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*"MJPG"))
         sleep(0.1)
-        if not self.video_capture.isOpened():
-            print("Cannot open camera")
-            exit()
         return self
 
     def __exit__(self, exc_type, exc_value, tb):
-        self.video_capture.release()
+        if self.video_capture:
+            self.video_capture.release()
         self.video_capture = None
-        self.is_open = self.video_capture.isOpened() if self.video_capture else False
+        self.is_open = False
 
-    def stream(self, show=True):
-        # t_end = time() + duration_seconds
-
+    def stream(self, show=False):
+        # if not self.is_open:
+        #     raise
         ret, frame = self.video_capture.read()
+        if not ret:
+            print("stream has ended or there is an error")
+            self.__exit__(None, None, None)
         if show:
             cv.imshow("frame", frame)
             cv.waitKey(1)
--- video_source/video_source_interface.py
+++ video_source/video_source_interface.py
@@ -3,3 +3,15 @@ from abc import ABC, abstractclassmethod
 
 class VideoSource(ABC):
     pass
+
+    @abstractclassmethod
+    def __enter__(self):
+        pass
+
+    @abstractclassmethod
+    def __exit__(self, exc_type, exc_value, tb):
+        pass
+
+    @abstractclassmethod
+    def stream(self):
+        pass
